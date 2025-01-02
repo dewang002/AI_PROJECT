3955c150af48f2e418e6707316b4b0decb1c549e
Dewang601 <2002thapa@gmail.com>
2025-01-02 23:04:51 +0530

    db fix

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,22 +1,7 @@
-// import mongoose from "mongoose";
-
-// function connect() {
-//     const uri = process.env.MONGODB_URI.replace('<db_password>', process.env.MONGODB_PASSWORD);
-    
-//     return mongoose.connect(uri)
-//         .then(() => {
-//             console.log("Connected to MongoDB");
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         });
-// }
-
-// export default connect;
 
 import mongoose from "mongoose";
 function connect() {
-    const uri = ('mongodb+srv://new_user2:manishbisht1234@cluster0.ipvnrik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    const uri = process.env.MONGODB_URI
     return mongoose.connect(uri)
         .then(() => {
             console.log("Connected to MongoDB");
