eb8c47df3995c5881ed0b873b0e75864bc354962
Manish Singh Bisht <manisbst123@gmail.com>
2025-01-02 12:06:30 +0530

    adding mongodb url and passwrod string

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 function connect() {
-    mongoose.connect(process.env.MONGODB_URI)
+    const uri = process.env.MONGODB_URI.replace('<db_password>', process.env.MONGODB_PASSWORD);
+    
+    return mongoose.connect(uri)
         .then(() => {
             console.log("Connected to MongoDB");
         })
         .catch(err => {
             console.log(err);
-        })
+        });
 }
 
 export default connect;
