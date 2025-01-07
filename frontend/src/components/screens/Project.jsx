35321024f2ef1c3b4cb2291c4bfe44c8ad7e2942
Dewang601 <2002thapa@gmail.com>
2025-01-07 14:49:17 +0530

    chat done

--- frontend/src/components/screens/Project.jsx
+++ frontend/src/components/screens/Project.jsx
@@ -1,4 +1,10 @@
-import React, { Fragment, useEffect, useRef, useState,useContext } from "react";
+import React, {
+  Fragment,
+  useEffect,
+  useRef,
+  useState,
+  useContext,
+} from "react";
 import { useLocation } from "react-router-dom";
 import { FaUserGroup } from "react-icons/fa6";
 import { GrSend } from "react-icons/gr";
@@ -6,7 +12,11 @@ import { IoIosCloseCircleOutline } from "react-icons/io";
 import { CiUser } from "react-icons/ci";
 import { IoMdPersonAdd } from "react-icons/io";
 import axios from "../../config/axios";
-import { initializeSocket,reciveMessage,sendMessage } from "../../config/socket";
+import {
+  initializeSocket,
+  reciveMessage,
+  sendMessage,
+} from "../../config/socket";
 import UserContext from "../../context/User.context";
 const Project = () => {
   const location = useLocation();
@@ -14,11 +24,12 @@ const Project = () => {
   const [modal, setModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState([]);
   const [users, setUsers] = useState([]);
-  const [message,setMessage] = useState('')
+  const [message, setMessage] = useState("");
   const [projects, setProjects] = useState(location.state.elem);
   const groupListRef = useRef(null);
+  const messageBox = React.createRef();
 
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext);
 
   //this just add users in the popup modal
   const handleSelect = (id) => {
@@ -44,21 +55,22 @@ const Project = () => {
         console.log("error while adding collaborators " + err);
       });
   };
-  
-  const send=()=>{
-    sendMessage("project-message",{
+
+  const send = () => {
+    sendMessage("project-message", {
       message,
-      sender:user
-    })
-  }
+      sender: user,
+    });
+    appendOutGoingMessage(message)
+    setMessage("");
+  };
 
   useEffect(() => {
+    initializeSocket(projects._id);
 
-    initializeSocket(projects._id)
-
-    reciveMessage('project-message',data=>{
-      console.log(data)
-    })
+    reciveMessage("project-message", (data) => {
+      appendMessage(data);
+    });
 
     axios
       .get("/users/all")
@@ -93,15 +105,58 @@ const Project = () => {
     };
   });
 
+  const appendMessage = (messageObject) => {
+    const messageBox = document.querySelector(".messages");
+    const message = document.createElement("div");
+    message.classList.add(
+      "message",
+      "bg-white",
+      "w-fit",
+      "max-w-[55%]",
+      "flex",
+      "flex-col",
+      "p-1",
+      "m-2",
+      "rounded"
+    );
 
-  const appendMessage = (messageObject) =>{
-    
-  }
+    message.innerHTML = `
+    <small className="text-md font-bold">${messageObject.sender.email}</small>
+              <h4 className=" bg-white p-1 rounded-lg">
+                ${messageObject.message}
+              </h4>
+    `;
+    messageBox.appendChild(message);
+  };
+  const appendOutGoingMessage = (messageObject) => {
+    const messageBox = document.querySelector(".messages");
+    const message = document.createElement("div");
+    message.classList.add(
+      "message",
+      "bg-white",
+      "w-fit",
+      "max-w-[55%]",
+      "flex",
+      "flex-col",
+      "ml-auto",
+      "p-1",
+      "m-2",
+      "rounded"
+    );
+
+    message.innerHTML = `
+    <small className="text-md font-bold">${user.email}</small>
+              <h4 className=" bg-white p-1 rounded-lg">
+                ${messageObject}
+              </h4>
+    `;
+    messageBox.appendChild(message);
+  };
 
   return (
     <div className="h-screen w-screen flex">
-      <section className="relative sideBar flex flex-col max-w-[24%] h-full shadow-xl bg-zinc-300">
-        <header className="w-full h-16 border-b-[1px] border-black flex justify-between items-center p-4 ">
+      <section className="relative sideBar flex flex-col max-w-[24%] h-screen shadow-xl bg-zinc-300">
+        <header className="absolute z-10 top-0 w-full h-16 border-b-[1px] border-black flex justify-between items-center p-4 ">
           <button onClick={() => setModal(true)}>
             <IoMdPersonAdd size={24} />
           </button>
@@ -110,30 +165,21 @@ const Project = () => {
           </button>
         </header>
 
-        <div className="message_area cursor-default flex-grow flex flex-col ">
-          <div className="messages flex-grow overflow-y-auto flex flex-col p-2">
-            <div className="incoming_message">
-              <small className="text-md font-semibold">userGmail</small>
-              <h4 className="w-fit  max-w-[55%] bg-white p-1 rounded-lg">
-                hello this is Surbhi,adfadf df asdf asdf adsf asdf asdf asdf{" "}
-              </h4>
-            </div>
-            <div className="outgoing_message flex flex-col items-end ">
-              <small className="text-md font-semibold">userGmail</small>
-              <h4 className="w-fit max-w-[55%] bg-white p-1 rounded-lg">
-                kya likha hai ye sab btau abhi hai ..aldjfladjflaodjf
-                lasjdfoajsdl f
-              </h4>
-            </div>
+        <div className="message_area cursor-default h-full flex-grow flex flex-col pt-14 relative">
+          <div
+            ref={messageBox}
+            className="messages p-1 flex-grow flex flex-col gap-1 overflow-y-auto max-h-50vh">
           </div>
 
           <div className="message_input flex gap-4 w-full p-2">
             <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
               className="p-2 drop-shadow-xl w-full rounded"
               type="any"
               placeholder="type message..."
             />
-            <button value={message} onClick={send} className="flex-grow hover:scale-95">
+            <button onClick={send} className="flex-grow hover:scale-95">
               {<GrSend size={24} />}
             </button>
           </div>
@@ -146,7 +192,7 @@ const Project = () => {
           ref={groupListRef}
         >
           <header className="w-full h-16 border-b-[1px] border-black flex justify-between items-center p-4 ">
-           <h1 className="text-lg font-semibold">Collaborators</h1>
+            <h1 className="text-lg font-semibold">Collaborators</h1>
             <button onClick={() => setShowMember(false)}>
               <IoIosCloseCircleOutline size={24} />
             </button>
@@ -169,7 +215,7 @@ const Project = () => {
       </section>
 
       {modal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/45">
+        <div className="fixed inset-0 flex items-center justify-center bg-black/45">
           <div className=" bg-white w-1/5 rounded-lg shadow-lg p-6 ">
             <h2 className="text-lg font-bold mb-4">Add Collaborators</h2>
             <div className="max-h-[40vh] flex flex-col gap-2 overflow-auto">
