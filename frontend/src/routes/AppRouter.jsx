356f8a9862a35ee223c3355c7a3abf8a8b370fd3
Dewang <125146872+dewang002@users.noreply.github.com>
2025-01-16 12:00:31 +0530

    appyed lazy loading

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React,{lazy} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../components/screens/Login";
-import Register from "../components/screens/Register";
-const  Home = React.lazy(()=>import("../components/screens/Home"));
-import Project from "../components/screens/Project";
-import UserAuth from "../auth/UserAuth";
+const Login = lazy(()=>import("../components/screens/Login"));
+const Register = lazy(()=>import("../components/screens/Register")) ;
+const Home = lazy(()=>import("../components/screens/Home"));
+const Project = lazy(()=>import("../components/screens/Project") ) ;
+const UserAuth = lazy(()=>import("../auth/UserAuth")) ;
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Suspense fallback={"...loading"}> <UserAuth><Home /></UserAuth> </Suspense> } />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/project" element={ <UserAuth><Project /></UserAuth>} />
+        <Route path="/login" element={<Suspense fallback={"...loading"}><Login /></Suspense> } />
+        <Route path="/register" element={<Suspense fallback={"...loading"}><Register /></Suspense> } />
+        <Route path="/project" element={<Suspense fallback={"...loading"}><UserAuth><Project /></UserAuth></Suspense>} />
       </Routes>
     </BrowserRouter>
   );
